41ac3b04727eb160af3cd6fbfea3a9c5f3e2b4fa
Goran Topic <telegonicaxx@live.com>
2021-08-17 23:02:26 -0700

    adding pages and static files

--- blog-project/index.js
+++ blog-project/index.js
@@ -1,8 +1,16 @@
 const express = require('express'); // import express 
+const path = require('path'); // import path module
 
 const server = new express();
 
 // sever static file in the public directory
 server.use(express.static('public'));
 
-server.listen(4000, ()=> console.log('listeing on port 40000'));
+server.get('/', (request, response) => {  // if server get a get request
+		// sedn the file as a response from pages 
+		console.log(__dirname);
+		console.log('i ran');
+		response.sendFile( path.resolve( __dirname, 'pages/index.html') );
+});
+
+server.listen(4000, () => console.log('listeing on port 40000'));
