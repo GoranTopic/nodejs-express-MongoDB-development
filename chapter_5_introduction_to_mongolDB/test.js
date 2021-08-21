dfbbc90f6aca242a66c87bc28e9aa9c0cb431b3f
Goran Topic <telegonicaxx@live.com>
2021-08-20 17:48:30 -0700

    succesfully manage to connect to mongodb-compass

--- chapter_5_introduction_to_mongolDB/test.js
+++ chapter_5_introduction_to_mongolDB/test.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose'); // import mongoose module
 const BlogPost = require('./models/BlogPost'); // import blog post schema
 
-let url = 'mongodb://superlocalhost/my_database';
+let url = 'mongodb://127.0.0.1/my_database';
 
 
-mongoose.connect( url, function(err, db) {
-		if (err) throw err;
-		console.log("Database created!");
-		db.close();
-});
+mongoose.connect( url, { userNewUrlParser: true }); 
 
-/*
 BlogPost.create({
 		title:  'The Mythbuster guide to Saving more Money on Energy Bills', 
 		body: "And once the storm is over, you won't remember how you made it through, how you managed to survive. You won't even be sure whether the storm is really over. But one thing is certain. When you come out of the storm, you won't be the same person who walked in. That's what this storm's all about.", 
 }, (error, blogpot) => { console.log(error, blogpot) })
 
-*/
 
