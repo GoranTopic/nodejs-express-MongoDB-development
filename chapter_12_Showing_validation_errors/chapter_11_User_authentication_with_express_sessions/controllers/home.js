9095eed55ab5fc8575f480f85a137c90725abad5
Goran Topic <telegonicaxx@live.com>
2021-08-24 14:12:51 -0700

    actually finished chapter 11

---
+++ chapter_12_Showing_validation_errors/chapter_11_User_authentication_with_express_sessions/controllers/home.js
@@ -0,0 +1,9 @@
+const BlogPost = require('../models/BlogPost.js');
+
+module.exports = async(req, res) => {
+		/* get all the posts from the db and render them in index page */ 
+		const blogposts = await BlogPost.find({});
+		// return index view witht he blogpost passed
+		console.log(req.session);
+		res.render('index', { blogposts });
+}
